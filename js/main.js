04deb938a0ec474844b987b53645a40d14394ba0
Christian Frauenstein <cfrauenstein@gmail.com>
2016-05-14 18:03:40 +0200

    added some basic things to keep it a separate engine

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-import { Game } from './src/game';
+//your game init goes here
 
-var TAG = 'Main.js'
-
-//First app js to run
-var game = new Game();
-
-//A function to tell the rest of the HTML doc that the game has loaded.
-game.loadAssets(function (loader, resources) {
-
-    game.resources = resources;
-    console.log(TAG, ' done loading game assets.');
-    game.state_manager.fsm.loaded();
-
-});
+// import { Game } from './src/game';
+//
+// var TAG = 'Main.js'
+//
+// //First app js to run
+// var game = new Game();
+//
+// //A function to tell the rest of the HTML doc that the game has loaded.
+// game.loadAssets(function (loader, resources) {
+//
+//     game.resources = resources;
+//     console.log(TAG, ' done loading game assets.');
+//     game.state_manager.fsm.loaded();
+//
+// });
